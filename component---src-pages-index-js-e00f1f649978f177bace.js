3e9a7ddc10c2a5e95c092b0585ea102786103037
Albert Ho <albertcyi.ho@gmail.com>
2022-11-06 17:36:18 -0800

    Updates

---
+++ component---src-pages-index-js-e00f1f649978f177bace.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalbert=self.webpackChunkalbert||[]).push([[678],{6558:function(e,t,l){l.r(t);var n=l(7294);t.default=function(){return n.createElement("div",null,n.createElement("h1",null,"Hi, I'm Albert"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-e00f1f649978f177bace.js.map
